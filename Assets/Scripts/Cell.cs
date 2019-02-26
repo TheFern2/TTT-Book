f266692c3f2c993c69243bf7d4140051860e3371
fredz0003 <ic3balandran@yahoo.com>
2019-02-25 23:01:07 -0600

    Fixed Initial Setup without board.

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+
+    // Variables
+    private static int cellIndex;
+
+    // References for textures
+    public Sprite square_Empty;
+    public Sprite square_O;
+    public Sprite square_X;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
